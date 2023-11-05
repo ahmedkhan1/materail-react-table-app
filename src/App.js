d115253086b9ff16adaf905e8480ce0471fab2f2
Ahmed khan <ahmed_khan019@hotmail.com>
2023-11-05 23:14:54 +0500

    Added data array

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import { MaterialReactTable, useMaterialReactTable } from 'material-react-table';
 
+//recommended flat structure for data, but not required (nested data is fine, but takes more setup in column definitions)
+//must be memoized or stable (useState, useMemo, defined outside of the component, etc.)
+const data = [
+  {
+    name: 'John', // key "name" matches `accessorKey` in ColumnDef down below
+    age: 30, // key "age" matches `accessorKey` in ColumnDef down below
+  },
+  {
+    name: 'Sara',
+    age: 25,
+  },
+];
+
 function App() {
   return (
     <div className="App">
