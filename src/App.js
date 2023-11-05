b7cad00653dcd7be7e65d3e547db98fba410ff48
Ahmed khan <ahmed_khan019@hotmail.com>
2023-11-05 23:27:56 +0500

    Added heading

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ReactTable from './components/ReactTable';
 function App() {
   return (
     <div className="react-table-container">
+      <h1>Material React Table</h1>
       <ReactTable />
     </div>
   );
