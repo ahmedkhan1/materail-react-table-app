67891bbd415a1ec543673ab5f92f35e65652debb
Ahmed khan <ahmed_khan019@hotmail.com>
2023-11-05 23:19:27 +0500

    Moved material react table to ReactTable component

--- src/App.js
+++ src/App.js
@@ -1,51 +1,13 @@
-import { useMemo } from 'react';
-import {
-  MaterialReactTable,
-  useMaterialReactTable,
-} from 'material-react-table';
+import logo from './logo.svg';
+import './App.css';
+import ReactTable from './components/ReactTable';
 
-
-//mock data - strongly typed if you are using TypeScript (optional, but recommended)
-const data = [
-  {
-    name: 'John',
-    age: 30,
-  },
-  {
-    name: 'Sara',
-    age: 25,
-  },
-];
-
-export default function App() {
-  //column definitions - strongly typed if you are using TypeScript (optional, but recommended)
-  const columns = useMemo(
-    () => [
-      {
-        accessorKey: 'name', //simple recommended way to define a column
-        header: 'Name',
-        muiTableHeadCellProps: { style: { color: 'green' } }, //custom props
-      },
-      {
-        accessorFn: (originalRow) => originalRow.age, //alternate way
-        id: 'age', //id required if you use accessorFn instead of accessorKey
-        header: 'Age',
-        Header: <i style={{ color: 'red' }}>Age</i>, //optional custom markup
-      },
-    ],
-    [],
+function App() {
+  return (
+    <div className="App">
+      <ReactTable />
+    </div>
   );
-
-  //pass table options to useMaterialReactTable
-  const table = useMaterialReactTable({
-    columns,
-    data, //must be memoized or stable (useState, useMemo, defined outside of this component, etc.)
-    enableRowSelection: true, //enable some features
-    enableColumnOrdering: true,
-    enableGlobalFilter: false, //turn off a feature
-  });
-
-  //note: you can also pass table options as props directly to <MaterialReactTable /> instead of using useMaterialReactTable
-  //but that is not recommended and will likely be deprecated in the future
-  return <MaterialReactTable table={table} />;
 }
+
+export default App;
--- src/components/ReactTable.js
+++ src/components/ReactTable.js
@@ -1,7 +1,51 @@
-import React from 'react'
+import { useMemo } from 'react';
+import {
+  MaterialReactTable,
+  useMaterialReactTable,
+} from 'material-react-table';
+
+
+//mock data - strongly typed if you are using TypeScript (optional, but recommended)
+const data = [
+  {
+    name: 'John',
+    age: 30,
+  },
+  {
+    name: 'Sara',
+    age: 25,
+  },
+];
 
 export default function ReactTable() {
-  return (
-    <div>ReactTable</div>
-  )
+  //column definitions - strongly typed if you are using TypeScript (optional, but recommended)
+  const columns = useMemo(
+    () => [
+      {
+        accessorKey: 'name', //simple recommended way to define a column
+        header: 'Name',
+        muiTableHeadCellProps: { style: { color: 'green' } }, //custom props
+      },
+      {
+        accessorFn: (originalRow) => originalRow.age, //alternate way
+        id: 'age', //id required if you use accessorFn instead of accessorKey
+        header: 'Age',
+        Header: <i style={{ color: 'red' }}>Age</i>, //optional custom markup
+      },
+    ],
+    [],
+  );
+
+  //pass table options to useMaterialReactTable
+  const table = useMaterialReactTable({
+    columns,
+    data, //must be memoized or stable (useState, useMemo, defined outside of this component, etc.)
+    enableRowSelection: true, //enable some features
+    enableColumnOrdering: true,
+    enableGlobalFilter: false, //turn off a feature
+  });
+
+  //note: you can also pass table options as props directly to <MaterialReactTable /> instead of using useMaterialReactTable
+  //but that is not recommended and will likely be deprecated in the future
+  return <MaterialReactTable table={table} />;
 }
