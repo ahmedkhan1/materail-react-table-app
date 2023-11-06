a5f1f41042fe79fbb4fd3c631dfc717a14a242be
Ahmed khan <ahmed_khan019@hotmail.com>
2023-11-06 19:57:03 +0500

    Added Filter table example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import './App.css';
 import ReactTable from './components/ReactTable';
+
+
+
 
 function App() {
   return (
     <div className="react-table-container">
-      <h1 className='table-heading'>Material React Table Example</h1>
-      <ReactTable />
+      <h1 className='table-heading'>Material React Table Example</h1>      
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <ReactTable />
+      </LocalizationProvider>
     </div>
   );
 }
--- src/components/ReactTable.js
+++ src/components/ReactTable.js
@@ -3,31 +3,73 @@ import {
   MaterialReactTable,
   useMaterialReactTable,
 } from 'material-react-table';
+import { citiesList, data, usStateList } from './makeData';
 
-const data = [
-  {
-    name: 'John',
-    age: 30,
-  },
-  {
-    name: 'Sara',
-    age: 25,
-  },
-];
-
-export default function ReactTable() {
+const ReactTable = () => {
   const columns = useMemo(
     () => [
       {
-        accessorKey: 'name',
-        header: 'Name',
-        muiTableHeadCellProps: { style: { color: 'green' } }, 
+        header: 'Status',
+        accessorFn: (originalRow) => (originalRow.isActive ? 'true' : 'false'), //must be strings
+        id: 'isActive',
+        filterVariant: 'checkbox',
+        Cell: ({ cell }) =>
+          cell.getValue() === 'true' ? 'Active' : 'Inactive',
+        size: 170,
       },
       {
-        accessorFn: (originalRow) => originalRow.age,
-        id: 'age',
+        accessorKey: 'name',
+        header: 'Name',
+        filterVariant: 'text', // default
+        size: 200,
+      },
+      {
+        accessorFn: (originalRow) => new Date(originalRow.hireDate), //convert to date for sorting and filtering
+        id: 'hireDate',
+        header: 'Hire Date',
+        filterVariant: 'date-range',
+        Cell: ({ cell }) => cell.getValue().toLocaleDateString(), // convert back to string for display
+      },
+      {
+        accessorKey: 'age',
         header: 'Age',
-        Header: <i style={{ color: 'red' }}>Age</i>, 
+        filterVariant: 'range',
+        filterFn: 'between',
+        size: 80,
+      },
+      {
+        accessorKey: 'salary',
+        header: 'Salary',
+        Cell: ({ cell }) =>
+          cell.getValue().toLocaleString('en-US', {
+            style: 'currency',
+            currency: 'USD',
+          }),
+        filterVariant: 'range-slider',
+        filterFn: 'betweenInclusive', // default (or between)
+        muiFilterSliderProps: {
+          marks: true,
+          max: 200_000, //custom max (as opposed to faceted max)
+          min: 30_000, //custom min (as opposed to faceted min)
+          step: 10_000,
+          valueLabelFormat: (value) =>
+            value.toLocaleString('en-US', {
+              style: 'currency',
+              currency: 'USD',
+            }),
+        },
+      },
+      {
+        accessorKey: 'city',
+        header: 'City',
+        filterVariant: 'select',
+        filterSelectOptions: citiesList, //custom options list (as opposed to faceted list)
+      },
+      {
+        accessorKey: 'state',
+        header: 'State',
+        filterVariant: 'multi-select',
+        filterSelectOptions: usStateList, //custom options list (as opposed to faceted list)
       },
     ],
     [],
@@ -36,10 +78,11 @@ export default function ReactTable() {
   const table = useMaterialReactTable({
     columns,
     data,
-    enableRowSelection: true, 
-    enableColumnOrdering: true,
-    enableGlobalFilter: false,
+    initialState: { showColumnFilters: true },
   });
 
-  return <MaterialReactTable table={table} className="react-table" />;
-}
+  return <MaterialReactTable table={table} />;
+};
+
+
+export default ReactTable;
